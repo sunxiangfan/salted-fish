6cf9fc25f46d43033fb4f389d9350142890b6de4
sunxiangfan <13111110495@163.com>
2019-04-18 21:06:37 +0800

    1

--- src/main/java/com/salted/fish/controller/FishRotationController.java
+++ src/main/java/com/salted/fish/controller/FishRotationController.java
@@ -1,9 +1,18 @@
 package com.salted.fish.controller;
 
+import com.salted.fish.common.dto.ResultDTO;
+import com.salted.fish.common.entity.FishRotation;
 import com.salted.fish.service.IFishRotationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 轮播图配置 信息操作处理
@@ -13,10 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RefreshScope
 @RestController
+@RequestMapping("web")
 public class FishRotationController {
 
+    Logger log = LoggerFactory.getLogger(FishRotationController.class);
 
     @Autowired
     private IFishRotationService fishRotationService;
 
+    @CrossOrigin
+    @PostMapping("selectFishRotationList")
+    public ResultDTO selectFishRotationList() {
+        log.info("selectFishRotationList");
+        try {
+            FishRotation fishRotation = new FishRotation();
+            fishRotation.setIsRelease("1");
+            List<FishRotation> fishRotations = fishRotationService.selectFishRotationList(fishRotation);
+            return ResultDTO.success(fishRotations);
+        } catch (Exception e) {
+            log.error("selectFishRotationList error", e);
+            return ResultDTO.fail();
+        }
+    }
 }
