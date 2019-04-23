2d6c4eefa5f9e53cb5fa25ec7da7530301228867
sunxiangfan <13111110495@163.com>
2019-04-23 14:56:26 +0800

    1

--- src/main/java/com/salted/fish/controller/FishVideoChapterController.java
+++ src/main/java/com/salted/fish/controller/FishVideoChapterController.java
@@ -1,9 +1,15 @@
 package com.salted.fish.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.salted.fish.common.dto.ResultDTO;
+import com.salted.fish.common.entity.FishVideoChapter;
 import com.salted.fish.service.IFishVideoChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 章节 信息操作处理
@@ -12,12 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-04-19
  */
 @RestController
-@RequestMapping("/system/fishVideoChapter")
+@RequestMapping("web")
 public class FishVideoChapterController {
 
+    Logger log = LoggerFactory.getLogger(FishVideoChapterController.class);
 
     @Autowired
     private IFishVideoChapterService fishVideoChapterService;
 
 
+    @CrossOrigin
+    @PostMapping("selectFishChapterList")
+    public ResultDTO selectFishChapterList(@RequestBody FishVideoChapter dto) {
+        log.info("selectFishChapterList：" + JSON.toJSONString(dto));
+        try {
+            List<FishVideoChapter> fishVideoChapterList = fishVideoChapterService.selectFishVideoChapterList(dto);
+            return ResultDTO.success(fishVideoChapterList);
+        } catch (Exception e) {
+            log.error("selectFishChapterList error", e);
+            return ResultDTO.fail();
+        }
+    }
 }
